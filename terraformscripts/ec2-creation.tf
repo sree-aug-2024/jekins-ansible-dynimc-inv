d45b47d5e0496d43bb87d820b37f337cb0e64596
jayasree874 <bommanjaya26@gmail.com>
2024-11-09 13:51:49 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -3,7 +3,7 @@ region = "ap-southeast-2"
 }
 
 resource "aws_instance" "AWSINSTANCE"{
-ami = ami-037a2314eeca55594
+ami = "ami-037a2314eeca55594"
 instance_type = "t2.micro"
 key_name = "Devopsjayasree"
 security_groups = ["launch-wizard-2"]
